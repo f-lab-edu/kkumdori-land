508c6651e9810a528164539d8e03e6e56fc66530
Lee seung chan <dltmdcks702@gmail.com>
2024-03-30 10:44:54 +0900

    init: java spring project

---
+++ src/main/java/org/example/kkumdoriland/KkumdoriLandApplication.java
@@ -0,0 +1,13 @@
+package org.example.kkumdoriland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkumdoriLandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkumdoriLandApplication.class, args);
+    }
+
+}
