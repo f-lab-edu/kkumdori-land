2df80ed0064dc2ee355cac1837a737e3eca80739
Lee seung chan <dltmdcks702@gmail.com>
2024-04-10 13:46:59 +0900

    feat: add feed entity

---
+++ src/main/java/org/example/kkumdoriland/feed/domain/Feed.java
@@ -0,0 +1,24 @@
+package org.example.kkumdoriland.feed.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.example.kkumdoriland.user.domain.User;
+
+@Entity
+public class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "jsonb")
+    private FeedContents contents;
+
+    @ManyToOne
+    @JoinColumn(name="authorId")
+    private User user;
+}
