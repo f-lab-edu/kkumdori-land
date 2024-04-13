96a45c36a6fa0dd774b2c4e96d58d92f6301dee9
Lee seung chan <dltmdcks702@gmail.com>
2024-04-13 17:04:35 +0900

    feat: initialize acceptance test

---
+++ src/test/java/org/example/kkumdoriland/utils/DatabaseCleanup.java
@@ -0,0 +1,41 @@
+package org.example.kkumdoriland.utils;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.metamodel.EntityType;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Profile("test")
+@Service
+public class DatabaseCleanup implements InitializingBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    @Override
+    public void afterPropertiesSet() {
+        tableNames = entityManager
+            .getMetamodel().getEntities().stream()
+            .filter(entity -> entity.getJavaType().getAnnotation(Entity.class) != null)
+            .map(EntityType::getName)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        for(String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        }
+        entityManager.createNativeQuery("SET REFERNTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+}
