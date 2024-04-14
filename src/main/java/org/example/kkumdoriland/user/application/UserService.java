3190ecf483481b5e219aaa35e06adfb36f053a27
Lee seung chan <dltmdcks702@gmail.com>
2024-04-14 18:55:42 +0900

    feat: add read only transaction

--- src/main/java/org/example/kkumdoriland/user/application/UserService.java
+++ src/main/java/org/example/kkumdoriland/user/application/UserService.java
@@ -7,8 +7,10 @@ import org.example.kkumdoriland.user.exception.UserErrorCode;
 import org.example.kkumdoriland.user.exception.UserException;
 import org.example.kkumdoriland.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
