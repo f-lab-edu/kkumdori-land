f9043c374ef04e529d23570a4c91011613af959b
Lee seung chan <dltmdcks702@gmail.com>
2024-04-14 13:57:16 +0900

    feat: add web security config to work correctly

--- src/main/java/org/example/kkumdoriland/common/config/WebSecurityConfig.java
+++ src/main/java/org/example/kkumdoriland/common/config/WebSecurityConfig.java
@@ -2,7 +2,10 @@ package org.example.kkumdoriland.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,16 +14,13 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/user/join").permitAll()
+            .csrf(AbstractHttpConfigurer::disable)
+            .headers(header -> header.frameOptions(FrameOptionsConfig::sameOrigin))
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers(HttpMethod.POST, "/user/join").permitAll()
                 .anyRequest().authenticated()
-            )
-            .formLogin((form) -> form
-                .loginPage("/user/login")
-                .permitAll()
-            )
-            .logout((logout) -> logout.permitAll());
-
+            );
+        
         return http.build();
     }
 }
