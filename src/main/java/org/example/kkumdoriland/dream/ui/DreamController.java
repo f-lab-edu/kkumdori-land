278df05c168575eb67045336d5eb36a0c7450b78
Lee seung chan <dltmdcks702@gmail.com>
2024-05-06 23:28:18 +0900

    fix: make authentication provider work

--- src/main/java/org/example/kkumdoriland/dream/ui/DreamController.java
+++ src/main/java/org/example/kkumdoriland/dream/ui/DreamController.java
@@ -1,11 +1,12 @@
 package org.example.kkumdoriland.dream.ui;
 
+import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.example.kkumdoriland.auth.dto.AuthContext;
 import org.example.kkumdoriland.dream.dto.DreamCreateDTO;
 import org.example.kkumdoriland.dream.dto.DreamResponse;
 import org.example.kkumdoriland.dream.service.DreamService;
-import org.example.kkumdoriland.member.dto.MemberDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -20,11 +21,9 @@ public class DreamController {
     public final DreamService dreamService;
 
     @PostMapping()
-    public ResponseEntity<DreamResponse> createDream(@AuthenticationPrincipal MemberDTO member, @RequestBody DreamCreateDTO dto) {
-        final DreamResponse dream = dreamService.createDream(member.getId(), dto);
+    public ResponseEntity<DreamResponse> createDream(@AuthenticationPrincipal AuthContext authContext, @Valid @RequestBody DreamCreateDTO dto) {
+        final DreamResponse dream = dreamService.createDream(authContext.getMemberDTO().getId(), dto);
 
         return ResponseEntity.created(URI.create("/dream/" + dream.getId())).body(dream);
     }
-
-
 }
