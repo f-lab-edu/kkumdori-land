17589bb720e8da326a6b0bc4d06a371d0f9abd11
Lee seung chan <dltmdcks702@gmail.com>
2024-05-21 08:25:51 +0900

    refac: make acceptance step with builder

--- src/test/java/org/example/kkumdoriland/acceptance/domains/DreamSteps.java
+++ src/test/java/org/example/kkumdoriland/acceptance/domains/DreamSteps.java
@@ -1,71 +1,53 @@
 package org.example.kkumdoriland.acceptance.domains;
 
-import io.restassured.RestAssured;
+import io.restassured.http.Method;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.http.MediaType;
+import java.net.URI;
+import org.example.kkumdoriland.utils.AcceptanceStepBuilder;
 
 public class DreamSteps {
 
     public static ExtractableResponse<Response> 꿈_생성(String cookie, String title, String description, String dueDate) {
-        final Map<String, String> params = new HashMap<>();
-        params.put("title", title);
-        params.put("description", description);
-        params.put("dueDate", dueDate);
-
-        return RestAssured.given().log().all()
+        return AcceptanceStepBuilder.builder()
             .cookie("JSESSIONID", cookie)
-            .body(params)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/dream")
-            .then().log().all()
-            .extract();
+            .param("title", title)
+            .param("description", description)
+            .param("dueDate", dueDate)
+            .uri(URI.create("/api/dream"))
+            .method(Method.POST)
+            .build();
     }
 
     public static ExtractableResponse<Response> 마일스톤_생성(String cookie, Long dreamId, String title, String description, int totalScoreToEarn, int minimumStepSize) {
-        final Map<String, String> params = new HashMap<>();
-        params.put("title", title);
-        params.put("description", description);
-        params.put("totalScoreToEarn", Integer.toString(totalScoreToEarn));
-        params.put("minimumStepSize", Integer.toString(minimumStepSize));
-        params.put("dreamId", Long.toString(dreamId));
-
-        return RestAssured.given().log().all()
+        return AcceptanceStepBuilder.builder()
             .cookie("JSESSIONID", cookie)
-            .body(params)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/dream/milestone")
-            .then().log().all()
-            .extract();
+            .param("title", title)
+            .param("description", description)
+            .param("totalScoreToEarn", Integer.toString(totalScoreToEarn))
+            .param("minimumStepSize", Integer.toString(minimumStepSize))
+            .param("dreamId", Long.toString(dreamId))
+            .uri(URI.create("/api/dream/milestone"))
+            .method(Method.POST)
+            .build();
     }
 
     public static ExtractableResponse<Response> 하루기록_생성(String cookie, long milestoneId, String contentText, int stepSize) {
-        final Map<String, String> params = new HashMap<>();
-        params.put("contentText", contentText);
-        params.put("stepSize", Integer.toString(stepSize));
-        params.put("mileStoneId", Long.toString(milestoneId));
-
-        return RestAssured.given().log().all()
+        return AcceptanceStepBuilder.builder()
             .cookie("JSESSIONID", cookie)
-            .body(params)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/dream/milestone/dailyHistory")
-            .then().log().all()
-            .extract();
+            .param("contentText", contentText)
+            .param("stepSize", Integer.toString(stepSize))
+            .param("mileStoneId", Long.toString(milestoneId))
+            .uri(URI.create("/api/dream/milestone/dailyHistory"))
+            .method(Method.POST)
+            .build();
     }
 
     public static ExtractableResponse<Response> 꿈_조회(String cookie) {
-        return RestAssured.given().log().all()
+        return AcceptanceStepBuilder.builder()
             .cookie("JSESSIONID", cookie)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .get("/api/dream")
-            .then().log().all()
-            .extract();
+            .uri(URI.create("/api/dream"))
+            .method(Method.GET)
+            .build();
     }
 }
--- src/test/java/org/example/kkumdoriland/acceptance/domains/MemberSteps.java
+++ src/test/java/org/example/kkumdoriland/acceptance/domains/MemberSteps.java
@@ -1,39 +1,28 @@
 package org.example.kkumdoriland.acceptance.domains;
 
-import io.restassured.RestAssured;
+import io.restassured.http.Method;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.http.MediaType;
+import java.net.URI;
+import org.example.kkumdoriland.utils.AcceptanceStepBuilder;
 
 public class MemberSteps {
     public static ExtractableResponse<Response> 유저_생성(String name, String email, String password) {
-        final Map<String, String> params = new HashMap<>();
-        params.put("name", name);
-        params.put("email", email);
-        params.put("password", password);
-
-        return RestAssured.given().log().all()
-            .body(params)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/user/join")
-            .then().log().all()
-            .extract();
+        return AcceptanceStepBuilder.builder()
+            .param("name", name)
+            .param("email", email)
+            .param("password", password)
+            .uri(URI.create("/api/user/join"))
+            .method(Method.POST)
+            .build();
     }
 
     public static ExtractableResponse<Response> 유저_로그인(String email, String password) {
-        final Map<String, String> params = new HashMap<>();
-        params.put("email", email);
-        params.put("password", password);
-
-        return RestAssured.given().log().all()
-            .body(params)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/user/login")
-            .then().log().all()
-            .extract();
+        return AcceptanceStepBuilder.builder()
+            .param("email", email)
+            .param("password", password)
+            .uri(URI.create("/api/user/login"))
+            .method(Method.POST)
+            .build();
     }
 }
