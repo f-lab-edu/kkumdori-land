b9bfe5bec243c8709ada9e726bb421c0708e5fe6
crispy <dltmdcks702@cau.ac.kr>
2024-06-08 07:23:27 +0900

    feat: add milestone impl to get it with member

--- src/main/java/org/example/kkumdoriland/dream/repository/MileStoneRepository.java
+++ src/main/java/org/example/kkumdoriland/dream/repository/MileStoneRepository.java
@@ -3,6 +3,6 @@ package org.example.kkumdoriland.dream.repository;
 import org.example.kkumdoriland.dream.domain.MileStone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MileStoneRepository extends JpaRepository<MileStone, Long> {
+public interface MileStoneRepository extends JpaRepository<MileStone, Long>, MileStoneRepositoryCustom {
 
 }
---
+++ src/main/java/org/example/kkumdoriland/dream/repository/MileStoneRepositoryImpl.java
@@ -0,0 +1,24 @@
+package org.example.kkumdoriland.dream.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.kkumdoriland.dream.domain.MileStone;
+
+import static org.example.kkumdoriland.dream.domain.QDream.dream;
+import static org.example.kkumdoriland.dream.domain.QMileStone.mileStone;
+import static org.example.kkumdoriland.member.domain.QMember.member;
+
+@RequiredArgsConstructor
+public class MileStoneRepositoryImpl implements MileStoneRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+
+    @Override
+    public MileStone findByIdWithMember(Long id) {
+        return jpaQueryFactory.selectFrom(mileStone)
+                .leftJoin(mileStone.dream, dream)
+                .leftJoin(dream.user, member)
+                .where(mileStone.id.eq(id))
+                .fetchOne();
+    }
+}
